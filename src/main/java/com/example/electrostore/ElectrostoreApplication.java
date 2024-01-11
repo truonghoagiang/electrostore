49027f61422e548da53df82857c8011e29925ac0
Hoa Giang <giangtruonghoa@gmail.com>
2024-01-11 07:09:46 +0700

    truonghoagiang

---
+++ src/main/java/com/example/electrostore/ElectrostoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.electrostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectrostoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectrostoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/electrostore/ElectrostoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electrostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectrostoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
